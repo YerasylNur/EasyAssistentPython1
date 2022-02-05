02283617222efede81a702ec40e8b97cd5996c4f
YerasylNur <98863967+YerasylNur@users.noreply.github.com>
2022-02-05 22:34:54 +0600

    Add files via upload

---
+++ tester2.py
@@ -0,0 +1,10 @@
+while True:
+	code1 = input("Enter Command: ")
+	if code1 == "hi":
+		print("hello!")
+	if code1 == "who are you":
+		print("I am Robot")
+	if code1 == "Do you speak spanish":
+		print("yes")
+	if code1 == "exit":
+		exit()
